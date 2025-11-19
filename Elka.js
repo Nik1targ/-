c007ceca528695dbf8ddffebeed56d0651bfea91
Nik1targ <roan.forge@mail.ru>
2025-11-19 18:51:11 +0300

    add file

--- Elka.js
+++ Elka.js
@@ -20,4 +20,22 @@ for (let student of students) {
     console.log(student.name);
 }
 
+let pets = ["kot","pec","popygai"];
 
+console.log(pets.includes("pec"));
+console.log(pets.includes("homak"));
+
+
+let nums = [10,20,30,40];
+let part = nums.slice(1,3);
+
+console.log(part);
+console.log(nums);
+
+
+let numss = [10,20,30,40,50];
+
+let removed = numss.splice(1,2);
+
+console.log(numss);
+console.log(removed);
