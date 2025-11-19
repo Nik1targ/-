564b273e3f42c25ec593167ccb71e1f48e2d5e1e
Nik1targ <roan.forge@mail.ru>
2025-11-19 18:41:16 +0300

    add file

---
+++ Elka.js
@@ -0,0 +1,23 @@
+let numbers = [10,20,30];
+
+for (let i=0; i < numbers.length; i++){
+    console.log(numbers[i]);
+}
+
+
+let fruits = ["яблоко","кабан","груша"];
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+let students = [
+    {name:"Anna", age: 15},
+    {name:"Igor", age: 16}
+];
+
+for (let student of students) {
+    console.log(student.name);
+}
+
+
