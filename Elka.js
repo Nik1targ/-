2f698813019c72aa6c9de393f50d5fb7d26c9027
Nik1targ <roan.forge@mail.ru>
2025-11-24 17:39:58 +0300

    add file

--- Elka.js
+++ Elka.js
@@ -39,3 +39,46 @@ let removed = numss.splice(1,2);
 
 console.log(numss);
 console.log(removed);
+
+
+
+let nams = [1,2,3];
+let newNums = nams.map(n => n+1);
+
+console.log(newNums);
+
+
+
+let namz = [1,2,3];
+let newNumz = namz.map(n => n+5);
+
+console.log(newNumz);
+
+
+let namms = [1,78, -7, 2,37,4,5,6,10,32];
+
+let even = namms.filter(n => n > 10);
+
+console.log(even);
+
+
+
+let boxes = [
+    {weight: 2, label: "Легкая"},
+    {weight: 5, label: "Средняя"},
+    {weight: 12, label: "Тяжелая"},
+    {weight: 15, label: "Очень тяжелая"}
+];
+
+let heavyBox = boxes.find(box => box.weight >10);
+
+console.log(heavyBox);
+
+
+
+
+let innerPlanets = ["Меркурий","Венера","Земля","Марс"];
+let outerPlanets = ["юпитер","Сатурн","Уран","Нептун"];
+
+let allPlanets = innerPlanets.concat(outerPlanets);
+console.log(allPlanets);
